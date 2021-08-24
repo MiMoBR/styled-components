d2fdbaf6e7ab14274e4547ed312bf6cf7e20e75b
Reinaldo Nani <reinaldo@mimobr.com>
2021-08-24 10:41:09 -0300

    Create global.js

---
+++ global.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500&display=swap');
+
+html {
+    --backgroundWhite:rgb(255 255 255 / 80%);
+    --boxShadow:rgb(0 0 0 / 12%);
+}
+`
